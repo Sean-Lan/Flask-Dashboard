a83fc7b4b3751bd9bf93e0a53959fb2be8208181
Sean-Lan <sean.lan.thu@gmail.com>
2015-07-28 10:46:36 +0800

    Finish myRIO/roboRIO 2016 dashboard.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
         abort, render_template, flash
 from contextlib import closing
+from model.DBUtilities import Model
 
 # create application
 app = Flask(__name__)
@@ -12,7 +13,19 @@ def index():
 
 @app.route('/myrio_roborio_2016_stack_dashboard')
 def myrio_roborio_2016_stack_dashboard():
-    return render_template('myrio_roborio_2016_stack_dashboard.html')
+    model = Model('myrio_roborio_2016_stack_dashboard')
+    column_list = ['validated_stack', 'validated_stack_url', 'lv_version', 'lv_api_version', 
+            'safemode', 'sanity_test_result', 'sanity_test_result_url', 'comment']
+    records = model.select(column_list)
+    records = sorted(records, key=lambda record: record['validated_stack'], reverse=True)
+    return render_template('myrio_roborio_2016_stack_dashboard.html', records=records)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # model = Model('myrio_roborio_2016_stack_dashboard')
+    # column_list = ['validated_stack', 'validated_stack_url', 'lv_version', 'lv_api_version', 
+    #         'safemode', 'sanity_test_result', 'sanity_test_result_url', 'comment']
+    # records = model.select(column_list)
+    # records = sorted(records, key=lambda record: record['validated_stack'], reverse=True)
+    # for record in records:
+    #     print record
---
+++ main_config.py
@@ -0,0 +1,1 @@
+STACK_WEB_URL = 'http://niweb.natinst.com/confluence/display/RIOSW/NI-RIO+15.0.0+Install+Instructions'
